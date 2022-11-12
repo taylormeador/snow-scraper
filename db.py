574ad8e1d46fdc4e40b1c8fe27745d1ce0ba91eb
Taylor Meador <taylor.r.meador@gmail.com>
2022-11-11 19:30:20 -0700

    Refactor scraper and bug fix

---
+++ db.py
@@ -0,0 +1,21 @@
+import psycopg2
+from config import POSTGRES_PASSWORD, POSTGRES_HOST
+
+
+class DB:
+    def __init__(self):
+        self.conn = psycopg2.connect(
+                    database="snow",
+                    user="postgres",
+                    password=POSTGRES_PASSWORD,
+                    host=POSTGRES_HOST,
+                    port='5432'
+                )
+        self.cur = self.conn.cursor()
+
+    def get_resorts_key(self, resort_name, resort_state):
+        self.cur.execute('SELECT resorts_key FROM resorts_dim WHERE resort_name = %s AND resort_state = %s', (resort_name, resort_state))
+        result = self.cur.fetchone()
+        if result:
+            return result[0]
+        return None
--- snocountry_scraper.py
+++ snocountry_scraper.py
@@ -1,29 +1,35 @@
 from bs4 import BeautifulSoup
 import requests
+from db import DB
+
+# initialize db connection
+db = DB()
 
 # This script scrapes info about the amount of snow each resort in CO received in the past 24 hours
 # eventually it will insert this info into a database which can be used for analysis
-URL = "https://www.snocountry.com/snow-reports/us/colorado"
+URL = "https://www.snocountry.com/snow-reports/us/colorado"  # TODO expand this to other states
 page = requests.get(URL)
 
 soup = BeautifulSoup(page.content, "html.parser")
 content = soup.find(id="sppb-addon-1572032128072")
 
-resorts = content.find_all('div', class_='resort-conditions')
-print('[')
-for resort_row in resorts:
+def get_snow_fall_info(resort_row):
     resort = resort_row.find('a', class_='resortcol-title').text
     resort_name = resort.split(',')[0].strip()
     resort_state = resort.split(',')[1].strip()
 
     snow_fall_div = resort_row.find('div', class_='snow-fall')
+    amount = 0
     if snow_fall_div:
         snow_fall = snow_fall_div.find('div', class_='m-b-5')
         for p in snow_fall.p:
             words = p.strip().split(' ')
             if words[0] == 'Snow':
                 continue
-            amount = int(words[0][:-1]) if words[2] == '24' else 0
+            if words[2] == '24':
+                amount = int(words[0][:-1])
+    return resort_name, resort_state, amount
 
-    print("'" + resort_name + "',")
-print(']')
+resorts = content.find_all('div', class_='resort-conditions')
+for resort_row in resorts:
+    print(get_snow_fall_info(resort_row))
